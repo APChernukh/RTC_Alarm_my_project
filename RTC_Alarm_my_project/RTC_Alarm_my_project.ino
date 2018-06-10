9674336f2114bcde54331081c1c7a0dacb882b95
Alexandr Chernykh <soloptur@live.com>
2018-06-10 21:23:27 +0700

    Добавьте файлы проекта.

---
+++ RTC_Alarm_my_project/RTC_Alarm_my_project.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RTC_Alarm_my_project.ino
+ Created:	10.06.2018 21:22:53
+ Author:	solop
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
